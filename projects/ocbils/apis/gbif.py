78ed4ef2dadb4d2812135f6e37a015ffde7c38ed
CJ Grady <cjgrady@gmail.com>
2020-07-28 14:55:36 -0500

    Add files via upload

---
+++ projects/ocbils/apis/gbif.py
@@ -0,0 +1,39 @@
+"""Module containing functions for interacting with GBIF APIs."""
+import urllib
+import requests
+
+# .............................................................................
+def get_gbif_accepted_name(name_str):
+    """Get the accepted name for a name string."""
+    other_filters = {'name': name_str.strip(), 'verbose': 'true'}
+    url = 'http://api.gbif.org/v1/species/match?{}'.format(
+        urllib.parse.urlencode(other_filters))
+    response = requests.get(url).json()
+    if response['status'].lower() in ('accepted', 'synonym'):
+        return (response['canonicalName'], response['speciesKey'])
+    return None, None
+
+
+# .............................................................................
+def get_points_from_gbif(taxon_key, max_points=MAX_POINTS):
+    """Get points from GBIF using the provided taxon key."""
+    offset = 0
+    other_filters = {
+        'taxonKey': taxon_key,
+        'limit': LOCAL_LIMIT,
+        'hasCoordinate': 'true',
+        'hasGeospatialIssue': 'false',
+        'offset': offset
+    }
+    url = 'http://api.gbif.org/v1/occurrence/search?{}'
+    ret_points = []
+    response = requests.get(
+        url.format(urllib.parse.urlencode(other_filters))).json()
+    ret_points.extend(response['results'])
+    while not response['endOfRecords'] and offset < max_points:
+        offset += LOCAL_LIMIT
+        other_filters['offset'] = offset
+        response = requests.get(
+            url.format(urllib.parse.urlencode(other_filters))).json()
+        ret_points.extend(response['results'])
+    return ret_points
---
+++ projects/ocbils/apis/powo.py
@@ -0,0 +1,32 @@
+"""This module provides access to plants of the world APIs."""
+import pykew.powo as powo
+from pykew.powo_terms import Filters, Name
+
+
+# .............................................................................
+def get_species_kew(fq_id):
+    """Get the species information from kew"""
+    result = powo.lookup(fq_id, include=['distribution'])
+    return result
+
+
+# .............................................................................
+def get_kew_ids_for_genus(genus_name):
+    """Get ids for kew queries"""
+    query = {Name.genus: genus_name}
+    results = powo.search(query, filters=Filters.accepted)
+    try:
+        return {res['name']: res['fqId'] for res in results}
+    except Exception as err:
+        raise err
+
+
+# .............................................................................
+def get_kew_id_for_species(species_name):
+    """Get id for kew queries"""
+    query = {Name.species: species_name}
+    results = powo.search(query, filters=Filters.accepted)
+    try:
+        return {res['name']: res['fqId'] for res in results}
+    except Exception as err:
+        raise err
